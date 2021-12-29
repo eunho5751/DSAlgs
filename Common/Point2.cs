141be8cf6c1ff952e0e3b919a08ac452ee7fb5ff
eunho5751 <eunho5751@naver.com>
2021-12-29 23:08:03 +0900

    Implemented Point2/Point3 types with their operator overloadings

---
+++ Common/Point2.cs
@@ -0,0 +1,21 @@
+﻿
+public struct Point2
+{
+    public Point2(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static Point2 operator+(Point2 lhs, float value) => new Point2(lhs.X + value, lhs.Y + value);
+    public static Point2 operator-(Point2 lhs, float value) => new Point2(lhs.X - value, lhs.Y - value);
+    public static Point2 operator*(Point2 lhs, float value) => new Point2(lhs.X * value, lhs.Y * value);
+    public static Point2 operator/(Point2 lhs, float value) => new Point2(lhs.X / value, lhs.Y / value);
+    public static Point2 operator+(Point2 lhs, Point2 rhs) => new Point2(lhs.X + rhs.X, lhs.Y + rhs.Y);
+    public static Point2 operator-(Point2 lhs, Point2 rhs) => new Point2(lhs.X - rhs.X, lhs.Y - rhs.Y);
+
+    public override string ToString() => $"({X}, {Y})";
+
+    public float X { get; set; }
+    public float Y { get; set; }
+}
---
+++ Common/Point3.cs
@@ -0,0 +1,23 @@
+﻿
+public struct Point3
+{
+    public Point3(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static Point3 operator+(Point3 lhs, float value) => new Point3(lhs.X + value, lhs.Y + value, lhs.Z + value);
+    public static Point3 operator-(Point3 lhs, float value) => new Point3(lhs.X - value, lhs.Y - value, lhs.Z - value);
+    public static Point3 operator*(Point3 lhs, float value) => new Point3(lhs.X * value, lhs.Y * value, lhs.Z * value);
+    public static Point3 operator/(Point3 lhs, float value) => new Point3(lhs.X / value, lhs.Y / value, lhs.Z / value);
+    public static Point3 operator+(Point3 lhs, Point3 rhs) => new Point3(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z);
+    public static Point3 operator-(Point3 lhs, Point3 rhs) => new Point3(lhs.X - rhs.X, lhs.Y - rhs.Y, lhs.Z - rhs.Z);
+
+    public override string ToString() => $"({X}, {Y}, {Z})";
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
